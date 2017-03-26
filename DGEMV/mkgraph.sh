e1cd90026b086f5dad19c2037b834f58ad6f0723
Alexey R <lexa260011@ngs.ru>
2017-03-26 23:11:44 +0100

    fix speedup

--- DGEMV/mkgraph.sh
+++ DGEMV/mkgraph.sh
@@ -4,6 +4,7 @@ set terminal png size 800,480 enhanced font 'Arial, 16'
 set style line 1 linecolor rgb 'red' linetype 1 linewidth 2
 set style line 2 linecolor rgb 'blue' linetype 1 linewidth 2
 set style line 3 linecolor rgb 'green' linetype 1 linewidth 2
+set style line 4 linecolor rgb 'cyan' linetype 1 linewidth 2
 
 set border linewidth 1
 set key top left
@@ -22,7 +23,8 @@ set mxtics
 set output 'result.png'
 plot "15000.dat" using 1:2 title "15000" with linespoints ls 1, \
 	"20000.dat" using 1:2 title "20000" with linespoints ls 3, \
-	"25000.dat" using 1:2 title "25000" with linespoints ls 2
+	"25000.dat" using 1:2 title "25000" with linespoints ls 2, \
+	"ideal.dat" using 1:2 title "ideal" with linespoints ls 4
 
 set output '15000_elements.png'
 plot "15000.dat" using 1:2 title "15000" with linespoints ls 1
