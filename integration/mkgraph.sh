65c8f051ca127c2b93b6795d9fff51ded3cde32c
Alexey R <lexa260011@ngs.ru>
2017-04-02 20:29:51 +0700

    Update mkgraph.sh

--- integration/mkgraph.sh
+++ integration/mkgraph.sh
@@ -4,7 +4,6 @@ set terminal png size 800,480 enhanced font 'Arial, 10'
 set style line 1 linecolor rgb 'red' linetype 1 linewidth 2
 set style line 2 linecolor rgb 'blue' linetype 1 linewidth 2
 set style line 3 linecolor rgb 'green' linetype 1 linewidth 2
-set style line 4 linecolor rgb 'cyan' linetype 1 linewidth 2
 
 set border linewidth 1
 set key top left
