6e0e12012763b6f3c31f506cdbc4c13a58e0098d
Alexey R <lexa260011@ngs.ru>
2017-10-11 14:21:56 +0000

    -

--- firstLab/src/firstLabAlltoall.c
+++ firstLab/src/firstLabAlltoall.c
@@ -15,9 +15,7 @@ int main(int argc,char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &commsize);
   MPI_Get_processor_name(procname, &len);
-  MPI_Request req[commsize - 1];
-  MPI_Request req1[commsize - 1];
-  MPI_Status stat[commsize - 1];
+  MPI_Request req[2];
   recvbuf = malloc(sizeof(char) * buffSize * (commsize - 1));
   sendbuf = malloc(sizeof(char) * buffSize);
 
@@ -30,16 +28,15 @@ int main(int argc,char **argv)
   double time = MPI_Wtime();
   for (i = 0; i < commsize; i++) {
     if (i == rank) continue;
-      MPI_Isend(&sendbuf, buffSize, MPI_CHAR, i, 0, MPI_COMM_WORLD, &(req[(i > rank ? i - 1 : i)]));
-      MPI_Irecv(&(recvbuf[(i > rank ? i - 1 : i) * buffSize]), buffSize, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE, &(req1[(i > rank ? i - 1 : i)]));
+      MPI_Isend(&sendbuf, buffSize, MPI_CHAR, i, 0, MPI_COMM_WORLD, &(req[0]));
+      MPI_Irecv(&(recvbuf[(i > rank ? i - 1 : i) * buffSize]), buffSize, MPI_CHAR, i, 0, MPI_COMM_WORLD, &(req[1]));
+      MPI_Waitall(2, req, MPI_STATUS_IGNORE);
   }
-  MPI_Waitall(commsize - 1, req, stat);
-  MPI_Waitall(commsize - 1, req1, stat);
 /*  MPI_Alltoall(sendbuf, buffSize, MPI_CHAR,
                recvbuf, buffSize, MPI_CHAR, MPI_COMM_WORLD);  */
   time = MPI_Wtime() - time;
 
-  printf("Process %d of %d on %s(\n%s\n%s\n) received message (%ld) with time \t= %.6lf\n", rank, commsize, procname,sendbuf,recvbuf, buffSize, time);
+  printf("Process %d of %d on %s(\n%s\n%s\n) received message (%ld) with time \t= %.6lf\n", rank, commsize, procname,sendrecvbuf, buffSize, time);
 
   //MPI_Send(&sendbuf, buffSize, MPI_CHAR, next, 0, MPI_COMM_WORLD);
   //MPI_Recv(&recvbuf, buffSize, MPI_CHAR, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
