fb8193b81b5f9317522a6ca4de44408366ae2f18
Alexey R <lexa260011@ngs.ru>
2017-10-12 09:02:15 +0000

    -

--- firstLab/src/firstLabGather.c
+++ firstLab/src/firstLabGather.c
@@ -18,7 +18,8 @@ int main(int argc,char **argv)
   MPI_Get_processor_name(procname, &len);
   if (rank == root) recvbuf = malloc(sizeof(char) * buffSize * (commsize - 1));
   else sendbuf = malloc(sizeof(char) * buffSize);
-  MPI_Request req[commsize];
+  MPI_Request req[commsize - 1];
+  MPI_Request req1;
   int i = 0;
   if (rank > 0) {
     for (i = 0; i < buffSize - 1; i++) {
@@ -32,13 +33,13 @@ int main(int argc,char **argv)
     for (i = 0; i < commsize; i++) {
       if (i == root) continue;
 //      MPI_Recv(&(recvbuf[(i > root ? i - 1 : i) * buffSize]), buffSize, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      MPI_Irecv(&(recvbuf[(i > root ? i - 1 : i) * buffSize]), buffSize, MPI_CHAR, i, 0, MPI_COMM_WORLD, &(req[i]));
+      MPI_Irecv(&(recvbuf[(i > root ? i - 1 : i) * buffSize]), buffSize, MPI_CHAR, i, 0, MPI_COMM_WORLD, &(req[(i > root ? i - 1 : i)]));
     }
+    MPI_Waitall(commsize - 1, req, MPI_STATUS_IGNORE);
   } else {
-    MPI_Isend(&sendbuf, buffSize, MPI_CHAR, root, 0, MPI_COMM_WORLD, &(req[root]));
+    MPI_Isend(sendbuf, buffSize, MPI_CHAR, root, 0, MPI_COMM_WORLD, &req1);
+    MPI_Wait(&req1, MPI_STATUS_IGNORE);
   }
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Waitall(commsize, req, MPI_STATUS_IGNORE);
 
 //  MPI_Gather(sendbuf, buffSize, MPI_CHAR, recvbuf, buffSize, MPI_CHAR, root, MPI_COMM_WORLD);
   time = MPI_Wtime() - time;
