ae7ebb0bcc956ffb438dee3bd5c516af16d0baef
Alexey R <lexa260011@ngs.ru>
2017-10-12 09:03:01 +0000

    -

--- firstLab/src/firstLabBroadcast.c
+++ firstLab/src/firstLabBroadcast.c
@@ -17,7 +17,8 @@ int main(int argc,char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &commsize);
   MPI_Get_processor_name(procname, &len);
-  MPI_Request req[commsize];
+  MPI_Request req[commsize - 1];
+  MPI_Request req1;
 
   int i = 0;
   if (rank == root) sendbuf = malloc(sizeof(char) * buffSize);
