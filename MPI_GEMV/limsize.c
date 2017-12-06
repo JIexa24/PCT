592c2c74d1ace4c5b8f89a4c14f562f99889aec7
Alexey R <lexa260011@ngs.ru>
2017-12-06 08:55:06 -0100

    -

--- MPI_GEMV/limsize.c
+++ MPI_GEMV/limsize.c
@@ -6,15 +6,15 @@ int main()
   long nod = 18;
   long p = 8 * nod;
   double limitmemorynode = (1 << 30);
-  double a1;
+  double a1, a2;
   while (1)
   {
-    a1 = 8*(n*n/p + 2 * n);
-    double a2 = 8*((n+5)*(n+5)/p + 2 * (n+5));
+    a1 = 8*(n*n+ 2 * n)/p ;
+    a2 = 8*((n+5)*(n+5)+ 2 * (n+5))/p ;
     if (a1 < limitmemorynode && a2 > limitmemorynode) break;
     n+=5;
   }
   
-  printf("%ld %ld %lf\n", n , p, limitmemorynode);
+  printf("%ld %ld %lf < %lf < %lf\n", n , p,a1/1024, limitmemorynode / 1024,a2/1024);
   return 0;
 }
