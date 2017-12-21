2096001a9065cdd765c3300455382929908d82e5
Alexey R <lexa260011@ngs.ru>
2017-12-21 21:52:23 +0700

    =

--- MPI_Course_Work_Gaussian_Determinant/mkgraph.sh
+++ MPI_Course_Work_Gaussian_Determinant/mkgraph.sh
@@ -12,8 +12,8 @@ set grid
 set mytics 10
 set format x "%1.0f"
 set format y "%.1f"
-set xlabel "Threads (x8)" font "Arial, 16"
-set ylabel "Speedup ( 1x8 )" font "Arial, 16"
+set xlabel "Processes" font "Arial, 16"
+set ylabel "Speedup" font "Arial, 16"
 set xtics 1 font "Arial, 10"
 set ytics 1 font "Arial, 10"
 set rmargin 4
@@ -21,5 +21,5 @@ set tmargin 2
 set mxtics
 
 set output 'result.png'
-plot "test.dat" using 1:2 title "parallel, n = 30650" with linespoints ls 3, \
+plot "test.dat" using 1:2 title "Parallel, n = 30650" with linespoints ls 3, \
 	"ideal.dat" using 1:2 title "Linear Speedup" with linespoints ls 1, \
