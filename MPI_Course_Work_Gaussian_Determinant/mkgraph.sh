25347ba7d705a82fae3cfc60ac81e5a8f0e70021
Alexey R <lexa260011@ngs.ru>
2017-12-23 09:37:53 +0600

    -

--- MPI_Course_Work_Gaussian_Determinant/mkgraph.sh
+++ MPI_Course_Work_Gaussian_Determinant/mkgraph.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/gnuplot
 set terminal png size 800,480 enhanced font 'Arial, 10'
 
-set style line 1 linecolor rgb 'red' linetype 1 linewidth 2
+set style line 1 linecolor rgb 'red' linetype 2 linewidth 2
 set style line 3 linecolor rgb 'blue' linetype 1 linewidth 2
 set style line 2 linecolor rgb 'green' linetype 1 linewidth 2
 
@@ -12,8 +12,8 @@ set grid
 set mytics 10
 set format x "%1.0f"
 set format y "%.1f"
-set xlabel "Processes" font "Arial, 16"
-set ylabel "Speedup" font "Arial, 16"
+set xlabel "Количество процессов" font "Arial, 16"
+set ylabel "Относительное ускорение" font "Arial, 16"
 set xtics 8 font "Arial, 10"
 set ytics 1 font "Arial, 10"
 set rmargin 4
@@ -21,5 +21,5 @@ set tmargin 2
 set mxtics
 
 set output 'result.png'
-plot "test.dat" using 1:2 title "Parallel, n = 30650" with linespoints ls 3, \
-	"ideal.dat" using 1:2 title "Linear Speedup" with linespoints ls 1, \
+plot "ideal.dat" using 1:2 title "Линейное ускорение" with linespoints ls 1, \
+	"test.dat" using 1:2 title "Параллельная версия, N = 30650" with linespoints ls 3, \
